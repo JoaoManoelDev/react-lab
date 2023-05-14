f6750261a14600a421cccd6fbb539dec3a21b321
João Manoel <joaomanoeldev@gmail.com>
2023-05-14 00:20:28 -0300

    forms avancados

---
+++ forms-avancados/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Form } from "@/components/Form"
+
+export default function Home() {
+  return (
+   <main className="h-screen flex items-center justify-center">
+    <Form />
+   </main>
+  )
+}
